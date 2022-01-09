9a62d46adeb3d6aa6ace7ca67bd259535f0e936a
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-01-09 20:06:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url === "/"){
+        res.end("hello. you intered home page")
+    }
+    if(req.url === "/about"){
+        res.end("you are in about page")
+    }
+
+    res.end(`
+        <h1>Oops!!!</h1>
+        <p>Siz hozir kalla qo'ydingiz. Yana boshqatta kalla qo'yishni xohlasangiz <a href="/">buyerni</a> bosing.</p>
+   `)
+})
+
+server.listen(5000)
